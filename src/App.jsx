a41fe8f1a13d2f0f690a7ce5f1718384b9128d38
yujiseok <koesijuy@gmail.com>
2022-12-09 12:24:21 +0900

    feat: layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { Routes, Route } from "react-router-dom";
+import Footer from "./componets/footer/Footer";
+import Header from "./componets/header/Header";
+import Home from "./pages/Home/Home";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { StyledMain, Container } from "./styles";
+
+const Home = () => {
+  return (
+    <StyledMain>
+      <Container>메인 페이지 💫</Container>
+    </StyledMain>
+  );
+};
+export default Home;
