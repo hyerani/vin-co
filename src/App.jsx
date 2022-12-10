4bd754942f37e0c5cca0a485dade3bf239f7e2b8
Kim JiYeong <zmswkfl@naver.com>
2022-12-10 18:03:54 +0900

    feat : html, css 기본구조1 완성
    로그인 되지 않았을 시 && 장바구니가 비었을 시

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "./componets/footer/Footer";
 import Header from "./componets/header/Header";
 import Main from "./componets/main/Main";
-import Home from "./pages/home/Home";
+import Home from "./pages/Home/Home";
 import Login from "./pages/login/Login";
+import Cart from "./pages/cart/Cart";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
     </div>
