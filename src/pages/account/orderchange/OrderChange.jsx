b477fdd3c44f1a18bf5e2bba5774dbffd3bccce6
hyerani <hran9404@naver.com>
2022-12-12 15:21:11 +0900

    feat: create orderchange jsx file

---
+++ src/pages/account/orderchange/OrderChange.jsx
@@ -0,0 +1,12 @@
+const OrderChange = () => {
+  return (
+    <>
+      <div className="content">
+        <div className="title">주문 조회</div>
+      </div>
+      <div className="detail">취소 내역이 없습니다.</div>
+    </>
+  );
+};
+
+export default OrderChange;
