1dc0c94d2b5c390796409cad172c32e1df37b6d5
hyerani <hran9404@naver.com>
2022-12-12 15:16:11 +0900

    feat: add Account file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NotFound from "./pages/404/NotFound";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Account from "./pages/account/Account";
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Home />} />
           <Route path="/about" element={<Home />} />
-          <Route path="/account" element={<Home />} />
+          <Route path="/account" element={<Account />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/cart" element={<Home />} />
