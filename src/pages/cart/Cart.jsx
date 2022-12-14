f1124f2c04d7886991b6c8be4fa54724ec781e5d
Kim JiYeong <zmswkfl@naver.com>
2022-12-14 23:45:43 +0900

    feat : 아이템 삭제 가능

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,42 +1,58 @@
 import styled from "styled-components";
+import { ImCancelCircle } from "react-icons/im";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { useState } from "react";
 
 const Container = styled.div`
   max-width: 1024px;
   margin-inline: auto;
 `;
 
-const Item3 = [
-  {
+localStorage.setItem(
+  "0",
+  JSON.stringify({
     name: "Polaroid SX-70 Land Camera",
     price: 80000,
     number: 1,
     photo: "https://cdn.imweb.me/thumbnail/20220207/0bef11156dcdb.jpg",
-  },
-  {
+  }),
+);
+localStorage.setItem(
+  "1",
+  JSON.stringify({
     name: "Le Corbusier LC2 Chair",
     price: 960000,
     number: 2,
     photo: "https://cdn.imweb.me/thumbnail/20220207/6c05fee561509.jpg",
-  },
-  {
+  }),
+);
+localStorage.setItem(
+  "2",
+  JSON.stringify({
     name: "Vintage Mini Televison",
     price: 154000,
     number: 3,
     photo: "https://cdn.imweb.me/thumbnail/20220207/85ea9b194b0c1.jpg",
-  },
-];
-localStorage.item = JSON.stringify(Item3);
-
+  }),
+);
 const bag = (
+  // () => {
+  //   const [length, setlength] = useState(localStorage.length);
+  //   const Decrease = () => {
+  //     setlength((prevNumber) => prevNumber - 1);
+  //   };
+  //   return (
   <div>
-    장바구니 <span>{Item3.length}</span>
+    장바구니 <span>{localStorage.length}</span>
   </div>
 );
+//   );
+// };
 
 const table1Title = (
   <div>
     <div>
-      {Item3 ? <input type="checkbox" /> : <span> </span>}
+      {localStorage ? <input type="checkbox" /> : <span> </span>}
       <div>상품 정보</div>
     </div>
     <div>수량</div>
@@ -45,21 +61,40 @@ const table1Title = (
   </div>
 );
 
+const lo = [];
+const arr = Array(localStorage.length)
+  .fill()
+  .map((arr, i) => {
+    return i;
+  });
+arr.map((x) => lo.push(JSON.parse(localStorage[x])));
+console.log(lo);
+
+const remove = (event) => {
+  if (event.target.closest(".del")) {
+    localStorage.removeItem(event.target.closest(".item").classList[1]);
+    event.target.closest(".item").remove();
+    lo.splice(`${event.target.closest(".item").classList[1]}`, 1);
+  }
+};
+
 const Cart = () => {
   return (
     <Container>
       {bag}
       {table1Title}
-      {Item3 ? (
-        Item3.map((x) => {
+      {localStorage ? (
+        lo.map((x) => {
           return (
-            <div key={Item3.indexOf(x)}>
+            <div key={lo.indexOf(x)} className={`item ${lo.indexOf(x)}`}>
               <div>
                 <input type="checkbox" />
                 <div>
                   <img src={x.photo} alt={x.name} />
                   <div>{x.name}</div>
-                  <button type="button">x</button>
+                  <button type="button" className="del" onClick={remove}>
+                    <ImCancelCircle />
+                  </button>
                 </div>
               </div>
               <div>
@@ -67,12 +102,15 @@ const Cart = () => {
                 <button type="button">옵션/수량 버튼</button>
               </div>
               <div>
-                <div>{x.price}.toLocaleString()원</div>
+                <div>{x.price.toLocaleString()}원</div>
                 <button type="button">바로 구매</button>
               </div>
               <div>
                 <div>
-                  무료<span>?</span>
+                  무료
+                  <span>
+                    <AiOutlineQuestionCircle />
+                  </span>
                 </div>
                 <div>택배</div>
               </div>
