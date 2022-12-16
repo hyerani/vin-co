aedf999ef4b26f3aecd9781e1059317ae69723b4
Hyojina <hjkim3009@gmail.com>
2022-12-16 15:07:06 +0900

    feat: Create Shop

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,13 @@
+// import { useEffect, useState } from "react";
+// import { instance } from "../../api/api";
+import { Container } from "./styles";
+
+const Shop = () => {
+  return (
+    <Container>
+      <div>Shop here.</div>
+    </Container>
+  );
+};
+
+export default Shop;
---
+++ src/pages/shop/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { containerStyle } from "../../styles/styleConstants";
+
+const Container = styled.div`
+  ${containerStyle}
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  align-items: center;
+`;
+
+export { Container };
